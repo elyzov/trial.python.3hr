75bc6f6a597bab503c49f2115f1008bcef863242
Evgeny Lyzov <elyzov@plesk.com>
2020-07-15 00:11:31 +0700

    Added CRUD for Product entity

--- app/endpoints/products.py
+++ app/endpoints/products.py
@@ -1,8 +1,24 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, current_app, make_response
+from sqlalchemy.orm.exc import NoResultFound
 
+from app import db
+from app.endpoints.invalid_usage import InvalidUsage
 from app.models.products import Product
 
 products_blueprint = Blueprint('products', __name__)
+
+
+def jsonify_no_content():
+    response = make_response('', 204)
+    response.mimetype = current_app.config['JSONIFY_MIMETYPE']
+    return response
+
+
+@products_blueprint.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 
 @products_blueprint.route('/products', methods=['GET'])
@@ -10,3 +26,47 @@ def get_products():
     return jsonify({
         'results': [p.serialized for p in Product.query.all()]
     })
+
+
+@products_blueprint.route('/products', methods=['POST'])
+def create_product():
+    payload = request.json
+    current_app.logger.debug(f'got payload = {payload}')
+    try:
+        product = Product.create(payload)
+        return jsonify(product.serialized), 201
+    except Exception as e:
+        raise InvalidUsage(f'An error accrued: {e}', 410)
+
+
+@products_blueprint.route('/products/<int:id>', methods=['PUT'])
+def update_product(id):
+    payload = request.json
+    current_app.logger.debug(f'got payload = {payload}')
+    try:
+        product = Product.update(id, payload)
+        return jsonify(product.serialized)
+    except NoResultFound:
+        raise InvalidUsage(f'Product with id {id} not found', 404)
+    except Exception as e:
+        raise InvalidUsage(f'An error accrued: {e}', 410)
+
+
+@products_blueprint.route('/products/<int:id>', methods=['GET'])
+def get_product(id):
+    try:
+        product = Product.retrieve(id)
+        return jsonify(product.serialized)
+    except NoResultFound:
+        raise InvalidUsage(f'Product with id {id} not found', 404)
+    except Exception as e:
+        raise InvalidUsage(f'An error accrued: {e}', 410)
+
+
+@products_blueprint.route('/products/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    try:
+        Product.delete(id)
+        return jsonify_no_content()
+    except Exception as e:
+        raise InvalidUsage(f'An error accrued: {e}', 410)
--- app/models/products.py
+++ app/models/products.py
@@ -1,5 +1,13 @@
+from flask import current_app
+from sqlalchemy.orm import validates
+
 from app import db
 import datetime
+
+
+class InvalidPayload(Exception):
+    """Raised when the got invalid payload"""
+    pass
 
 
 class Product(db.Model):
@@ -19,6 +27,38 @@ class Product(db.Model):
     items_in_stock = db.Column(db.Integer, nullable=False)
     receipt_date = db.Column(db.DateTime, nullable=True)
 
+    @validates('name')
+    def validate_name(self, key, name):
+        current_app.logger.debug(f'validate product name: key = {key}, name = {name}')
+        name_len = len(name)
+        if name_len < 0 or 50 < name_len:
+            raise ValueError(f'[name] expected length between 1-50, got {name_len}')
+        return name
+
+    @validates('featured')
+    def validate_featured(self, key, featured):
+        current_app.logger.debug(f'validate product featured: key = {key}, featured = {featured}')
+        if not isinstance(featured, bool):
+            raise ValueError(f'[featured] boolean is expected, but got {type(featured).__name__} ({featured})')
+        return featured
+
+    @validates('rating')
+    def validate_rating(self, key, rating):
+        current_app.logger.debug(f'validate product rating: key = {key}, rating = {rating}')
+        if rating < 0:
+            raise ValueError(f'[rating] expected positive value, got {rating}')
+        return rating
+
+    @validates('items_in_stock')
+    def validate_items_in_stock(self, key, items_in_stock):
+        current_app.logger.debug(f'validate product items_in_stock: key = {key}, items_in_stock = {items_in_stock}')
+        if items_in_stock < 0:
+            raise ValueError(f'[items_in_stock] expected positive value, got {items_in_stock}')
+        return items_in_stock
+
+    def __str__(self):
+        return f'({self.id}) {self.name}'
+
     @property
     def serialized(self):
         return {
@@ -34,6 +74,58 @@ class Product(db.Model):
             'created_at': self.created_at
         }
 
+    @classmethod
+    def prepare_values(cls, payload):
+        values = {k: v for k, v in payload.items() if k in cls.__table__.columns}
+        brand_name = payload.get('brand', None)
+        if brand_name:
+            brand = db.session.query(Brand).filter_by(name=brand_name).first()
+            if not brand:
+                raise InvalidPayload(f'got unknown brand {brand_name}')
+            values['brand_id'] = brand.id
+        categories = {
+            name: db.session.query(Category).filter_by(name=name).first() for name in payload.get('categories', [])
+        }
+        for name, category in categories.items():
+            if not category:
+                raise InvalidPayload(f'got unknown category {name}')
+        if categories:
+            values['categories'] = [category for _, category in categories.items()]
+        return values
+
+    @classmethod
+    def update(cls, id, payload):
+        values = cls.prepare_values(payload)
+        obj = db.session.query(Product).get(id)
+        for k, v in values.items():
+            setattr(obj, k, v)
+        db.session.commit()
+        return obj
+
+    @classmethod
+    def delete(cls, id):
+        obj = db.session.query(Product).get(id)
+        if obj:
+            current_app.logger.debug(f'got product for delete {obj}')
+            db.session.delete(obj)
+            db.session.commit()
+
+    @classmethod
+    def retrieve(cls, id):
+        return db.session.query(Product).filter_by(id=id).one()
+
+    @classmethod
+    def create(cls, payload):
+        values = cls.prepare_values(payload)
+        if not values.get('brand_id', None):
+            raise InvalidPayload('brand is required')
+        if not values.get('categories', []):
+            raise InvalidPayload('categories is required')
+        obj = cls(**values)
+        db.session.add(obj)
+        db.session.commit()
+        return obj
+
 
 class Brand(db.Model):
     __tablename__ = 'brands'
@@ -43,6 +135,9 @@ class Brand(db.Model):
     country_code = db.Column(db.Unicode(2), nullable=False)
 
     products = db.relationship('Product', backref='brand')
+
+    def __str__(self):
+        return f'({self.id}) {self.name}'
 
     @property
     def serialized(self):
@@ -58,6 +153,9 @@ class Category(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Unicode(50), nullable=False)
+
+    def __str__(self):
+        return f'({self.id}) {self.name}'
 
     @property
     def serialized(self):
